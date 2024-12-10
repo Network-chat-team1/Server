1fc4820b3af821ef5ea4d5c7530f8c753d06df7c
jihyeon baek <qormoon@sookmyung.ac.kr>
2024-12-11 00:34:17 +0900

    fix: 스웨거 설정 수정

--- src/main/java/network/config/SwaggerConfig.java
+++ src/main/java/network/config/SwaggerConfig.java
@@ -1,11 +1,13 @@
 package network.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -14,14 +16,22 @@ public class SwaggerConfig {
 	public OpenAPI SwaggerAPI() {
 		return new OpenAPI()
 			.components(new Components())
+			.servers(servers()) // 서버 정보를 추가
 			.info(apiInfo());
-
 	}
 
-	private Info apiInfo(){
+	private Info apiInfo() {
 		return new Info()
 			.title("Network Swagger")
 			.description("Network Server API 명세서")
 			.version("1.0.0");
+	}
+
+	private List<Server> servers() {
+		return List.of(
+			new Server()
+				.url("https://network-chat.store") // HTTPS URL 설정
+				.description("Production Server") // 설명 추가
+		);
 	}
 }
--- src/main/java/network/config/WebConfig.java
+++ src/main/java/network/config/WebConfig.java
@@ -8,10 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**") // 모든 URL에 대해 CORS 허용
-			.allowedOrigins("http://localhost:3000", "http://3.39.185.125", "https://hospitalchat.netlify.app", "https://network-chat.store") // 모든 도메인 허용
+		registry.addMapping("/**") // 모든 URL 허용
+			.allowedOrigins("*") // 모든 Origin 허용
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
 			.allowedHeaders("*") // 모든 헤더 허용
-			.allowCredentials(true); // 인증 정보 허용 (필요시 true, 기본은 false)
+			.allowCredentials(false); // 인증 정보 사용 안 함 (Postman에서는 보통 필요 없음)
 	}
 }
