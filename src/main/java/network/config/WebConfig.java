6ee3b7deddb17ed87081ac257844f8bdcc045b5e
jihyeon baek <qormoon@sookmyung.ac.kr>
2024-12-10 17:13:45 +0900

    fix: 도메인 명시적 지정

--- src/main/java/network/config/WebConfig.java
+++ src/main/java/network/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**") // 모든 URL에 대해 CORS 허용
-			.allowedOrigins("*") // 모든 도메인 허용
+			.allowedOrigins("http://localhost:3000", "http://3.39.185.125") // 모든 도메인 허용
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
 			.allowedHeaders("*") // 모든 헤더 허용
 			.allowCredentials(true); // 인증 정보 허용 (필요시 true, 기본은 false)
